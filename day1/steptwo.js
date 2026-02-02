525f848942e689a564eb124fec916e24a8ded8e9
ali bassim <ali.bassim@epitech.eu>
2026-02-02 14:19:24 +0100

    [add] step 2.1

---
+++ day1/steptwo.js
@@ -0,0 +1,17 @@
+let firstlet = 0;
+const firstconst  = 1;
+let string  = "ali is a man";
+let bool = true;
+let fruits = [];
+var ali = 1;
+
+
+fruits.push("banana", "apple", "berry");
+
+console.log(firstconst);
+console.log(firstlet);
+console.log(string);
+console.log(fruits);
+console.log(ali);
+
+
